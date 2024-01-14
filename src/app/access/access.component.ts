c8a4b080d9d111efefc2ceb202b7255d447e3e9f
Augusto Marsola <augusto.marsola@live.com>
2024-01-14 17:28:15 -0300

    Initiate app

---
+++ src/app/access/access.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-access',
+  standalone: true,
+  imports: [],
+  templateUrl: './access.component.html',
+})
+export class AccessComponent {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AccessComponent } from './access/access.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, AccessComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angu-ude-app3-instagram';
