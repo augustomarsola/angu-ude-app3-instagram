28559a250d0d0dda3005a6651543c3d753479a3b
Augusto Marsola <augusto.marsola@live.com>
2024-01-17 20:09:52 -0300

    Added banner component

--- src/app/access/access.component.ts
+++ src/app/access/access.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { BannerComponent } from './banner/banner.component';
 
 @Component({
   selector: 'app-access',
   standalone: true,
-  imports: [],
+  imports: [BannerComponent],
   templateUrl: './access.component.html',
 })
 export class AccessComponent {}
