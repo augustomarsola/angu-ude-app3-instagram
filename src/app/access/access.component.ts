3d5552d0c24a191a1b9866020fc0b00dfd9e26c1
Augusto Marsola <augusto.marsola@live.com>
2024-01-17 20:22:42 -0300

    Added login component

--- src/app/access/access.component.ts
+++ src/app/access/access.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from './banner/banner.component';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-access',
   standalone: true,
-  imports: [BannerComponent],
+  imports: [BannerComponent, LoginComponent],
   templateUrl: './access.component.html',
 })
 export class AccessComponent {}
