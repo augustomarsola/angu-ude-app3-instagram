8a8dd89d9211d2286e14a88d40c580cdcff8943c
Augusto Marsola <augusto.marsola@live.com>
2024-01-18 20:54:16 -0300

    Added register component

--- src/app/access/access.component.ts
+++ src/app/access/access.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from './banner/banner.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @Component({
   selector: 'app-access',
   standalone: true,
-  imports: [BannerComponent, LoginComponent],
+  imports: [BannerComponent, LoginComponent, RegisterComponent],
   templateUrl: './access.component.html',
 })
 export class AccessComponent {}
